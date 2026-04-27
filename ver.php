b62e71be1a0bda4fb35602e8f1e819c25dc8807b
Marcella Grangê <110675762+ilycella@users.noreply.github.com>
2026-04-27 20:34:09 -0300

    Delete ver.php

--- ver.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(file_exists("perguntas.txt")) {
- echo "<h1>Conteúdo do Arquivo TXT:</h1>";
- echo "<pre>" . file_get_contents("perguntas.txt") . "</pre>";
-} else {
- echo "O arquivo ainda não foi criado.";
-}
-?>
